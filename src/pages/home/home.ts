865ec35d002f7f45e04231984ea4f02bc9bbcb8c
vikingbr <lucasraphael.ti@gmail.com>
2018-06-11 15:07:16 -0300

    Adds httpCliente request for all car list

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,50 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
+import { Carro } from '../../modelos/carro';
+import { HttpErrorResponse } from '@angular/common/http';
+import { CarrosServiceProvider } from '../../providers/carros-service/carros-service';
+import { NavLifecycles } from '../../utils/ionic/nav/nav-lifecycles';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements NavLifecycles {
 
-  constructor(public navCtrl: NavController) {
+  public carros: Carro[];
 
+  constructor(public navCtrl: NavController,
+    private _loadingCtrl: LoadingController,
+    private _alertCtrl: AlertController,
+    private _carrosService: CarrosServiceProvider) {}
+
+  ionViewDidLoad() {
+    let loading = this._loadingCtrl.create({
+      content: 'Carregando carros...'
+    });
+
+    loading.present();
+
+    this._carrosService.lista()
+              .subscribe(
+                (carros) => {
+                  this.carros = carros;
+                  loading.dismiss();
+                },
+                (err: HttpErrorResponse) => {
+                  console.log(err);
+
+                  loading.dismiss();
+
+                  this._alertCtrl.create({
+                    title: 'Falha na conexão',
+                    subTitle: 'Não foi possível carregar a lista de carros. Tente novamente mais tarde!',
+                    buttons: [
+                      { text: 'Ok' }
+                    ]
+                  }).present();
+                }
+              );
   }
 
 }
